0f06cf2fb31500b2b63400986f7964912cacd2a9
rayadramez82 <148684166+rayadramez82@users.noreply.github.com>
2025-06-11 20:08:30 +0300

    commit

--- DEV/Angular/inventory-app.v25.1.2/src/app/app.component.ts
+++ DEV/Angular/inventory-app.v25.1.2/src/app/app.component.ts
@@ -2,16 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AsideNavBarComponent } from "./core/common/aside-nav-bar/aside-nav-bar.component";
 import { HeaderControlsComponent } from './core/common/header-controls/header-controls.component';
-import { InventoryModule } from './inventory/inventory.module';
-import { SearchInputComponent } from './core/common/controls/inputs/search-input/search-input.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     AsideNavBarComponent,
-    HeaderControlsComponent,
-    InventoryModule
+    HeaderControlsComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
