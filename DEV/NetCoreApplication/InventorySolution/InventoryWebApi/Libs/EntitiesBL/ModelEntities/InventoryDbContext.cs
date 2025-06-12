29fbeec84a4a6e0ae4d07590df4f54c8ba610cf8
rayadramez82 <148684166+rayadramez82@users.noreply.github.com>
2025-06-12 11:52:37 +0300

    commit

---
+++ DEV/NetCoreApplication/InventorySolution/InventoryWebApi/Libs/EntitiesBL/ModelEntities/InventoryDbContext.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InventoryWebApi.Libs.EntitiesBL.ModelEntities;
+
+public partial class InventoryDbContext : DbContext
+{
+    public InventoryDbContext()
+    {
+    }
+
+    public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<InventoryItemBrandCu> InventoryItemBrandCus { get; set; }
+
+    public virtual DbSet<InventoryItemCategoryCu> InventoryItemCategoryCus { get; set; }
+
+    public virtual DbSet<InventoryItemCu> InventoryItemCus { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<InventoryItemBrandCu>(entity =>
+        {
+            entity.ToTable("InventoryItemBrand_cu");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.ChartOfAccountCuId).HasColumnName("ChartOfAccount_CU_ID");
+            entity.Property(e => e.Description).HasMaxLength(200);
+            entity.Property(e => e.InsertedDate).HasColumnType("datetime");
+            entity.Property(e => e.InternalCode).HasMaxLength(50);
+            entity.Property(e => e.NameP)
+                .HasMaxLength(150)
+                .HasColumnName("Name_P");
+            entity.Property(e => e.NameS)
+                .HasMaxLength(150)
+                .HasColumnName("Name_S");
+        });
+
+        modelBuilder.Entity<InventoryItemCategoryCu>(entity =>
+        {
+            entity.ToTable("InventoryItemCategory_cu");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.ChartOfAccountCuId).HasColumnName("ChartOfAccount_CU_ID");
+            entity.Property(e => e.Description).HasMaxLength(200);
+            entity.Property(e => e.InsertedDate).HasColumnType("datetime");
+            entity.Property(e => e.InternalCode).HasMaxLength(50);
+            entity.Property(e => e.NameP)
+                .HasMaxLength(150)
+                .HasColumnName("Name_P");
+            entity.Property(e => e.NameS)
+                .HasMaxLength(150)
+                .HasColumnName("Name_S");
+        });
+
+        modelBuilder.Entity<InventoryItemCu>(entity =>
+        {
+            entity.ToTable("InventoryItem_cu");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.AbbreviationName).HasMaxLength(10);
+            entity.Property(e => e.AreaUnitMeasurePId).HasColumnName("AreaUnitMeasure_P_ID");
+            entity.Property(e => e.Code).HasMaxLength(200);
+            entity.Property(e => e.DefaultBarcode).HasMaxLength(200);
+            entity.Property(e => e.Description).HasMaxLength(200);
+            entity.Property(e => e.ExpirationDate).HasColumnType("datetime");
+            entity.Property(e => e.InDescription).HasMaxLength(100);
+            entity.Property(e => e.InsertedDate).HasColumnType("datetime");
+            entity.Property(e => e.InternalCode).HasMaxLength(150);
+            entity.Property(e => e.InventoryItemBrandCuId).HasColumnName("InventoryItemBrand_CU_ID");
+            entity.Property(e => e.InventoryItemCategoryCuId).HasColumnName("InventoryItemCategory_CU_ID");
+            entity.Property(e => e.InventoryItemInvoicingStrategyPId).HasColumnName("InventoryItemInvoicingStrategy_P_ID");
+            entity.Property(e => e.InventoryItemStatePurchasingTypePId).HasColumnName("InventoryItemStatePurchasingType_P_ID");
+            entity.Property(e => e.InventoryItemStateSellingTypePId).HasColumnName("InventoryItemStateSellingType_P_ID");
+            entity.Property(e => e.InventoryItemTypePId).HasColumnName("InventoryItemType_P_ID");
+            entity.Property(e => e.ItemSku)
+                .HasMaxLength(50)
+                .HasColumnName("ItemSKU");
+            entity.Property(e => e.NameP)
+                .HasMaxLength(150)
+                .HasColumnName("Name_P");
+            entity.Property(e => e.NameS)
+                .HasMaxLength(150)
+                .HasColumnName("Name_S");
+            entity.Property(e => e.OutDescription).HasMaxLength(100);
+            entity.Property(e => e.PurchasingDescription).HasMaxLength(100);
+            entity.Property(e => e.SellingEndDate).HasColumnType("datetime");
+            entity.Property(e => e.SellingStartDate).HasColumnType("datetime");
+            entity.Property(e => e.SizeUnitMeasurePId).HasColumnName("SizeUnitMeasure_P_ID");
+            entity.Property(e => e.VolumeUnitMeasurePId).HasColumnName("VolumeUnitMeasure_P_ID");
+            entity.Property(e => e.WeightUnitPId).HasColumnName("WeightUnit_P_ID");
+
+            entity.HasOne(d => d.InventoryItemBrandCu).WithMany(p => p.InventoryItemCus)
+                .HasForeignKey(d => d.InventoryItemBrandCuId)
+                .HasConstraintName("FK_InventoryItem_cu_InventoryItemBrand_cu");
+
+            entity.HasOne(d => d.InventoryItemCategoryCu).WithMany(p => p.InventoryItemCus)
+                .HasForeignKey(d => d.InventoryItemCategoryCuId)
+                .HasConstraintName("FK_InventoryItem_cu_InventoryItemCategory_cu");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DEV/NetCoreApplication/InventorySolution/InventoryWebApi/Libs/EntitiesBL/ModelEntities/InventoryItemCu.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventoryWebApi.Libs.EntitiesBL.ModelEntities;
+
+public partial class InventoryItemCu
+{
+    public int Id { get; set; }
+
+    public string Code { get; set; } = null!;
+
+    public bool? IsFavorite { get; set; }
+
+    public string NameP { get; set; } = null!;
+
+    public string? NameS { get; set; }
+
+    public bool? IsAvailable { get; set; }
+
+    public bool? IsCountable { get; set; }
+
+    public string? InternalCode { get; set; }
+
+    public string? ItemSku { get; set; }
+
+    public string? AbbreviationName { get; set; }
+
+    public string? DefaultBarcode { get; set; }
+
+    public int? InventoryItemCategoryCuId { get; set; }
+
+    public int? InventoryItemBrandCuId { get; set; }
+
+    public int InventoryItemStateSellingTypePId { get; set; }
+
+    public int InventoryItemStatePurchasingTypePId { get; set; }
+
+    public int? InventoryItemTypePId { get; set; }
+
+    public int InventoryItemInvoicingStrategyPId { get; set; }
+
+    public string? Description { get; set; }
+
+    public int? CustomerDaysLimit { get; set; }
+
+    public DateTime? SellingStartDate { get; set; }
+
+    public DateTime? SellingEndDate { get; set; }
+
+    public DateTime? ExpirationDate { get; set; }
+
+    public double? Length { get; set; }
+
+    public double? Width { get; set; }
+
+    public double? Height { get; set; }
+
+    public int? SizeUnitMeasurePId { get; set; }
+
+    public double? Area { get; set; }
+
+    public int? AreaUnitMeasurePId { get; set; }
+
+    public double? Volume { get; set; }
+
+    public int? VolumeUnitMeasurePId { get; set; }
+
+    public double? Weight { get; set; }
+
+    public int? WeightUnitPId { get; set; }
+
+    public bool? UseElectronicScale { get; set; }
+
+    public string? InDescription { get; set; }
+
+    public string? OutDescription { get; set; }
+
+    public double? DefaultSellingPrice { get; set; }
+
+    public double? MinSellingPrice { get; set; }
+
+    public double? PurchasingPrice { get; set; }
+
+    public string? PurchasingDescription { get; set; }
+
+    public bool IsOnDuty { get; set; }
+
+    public long? InsertedBy { get; set; }
+
+    public DateTime? InsertedDate { get; set; }
+
+    public virtual InventoryItemBrandCu? InventoryItemBrandCu { get; set; }
+
+    public virtual InventoryItemCategoryCu? InventoryItemCategoryCu { get; set; }
+}
