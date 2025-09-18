c773d5f48df7fbeee08081677b921866324d9d8c
rayadramez82 <148684166+rayadramez82@users.noreply.github.com>
2025-09-18 07:58:48 +0300

    new Solution

--- DEV/NetCoreApplication/InventorySolution/InventoryWebApi/Controllers/InventoryStoreController.cs
+++ DEV/NetCoreApplication/InventorySolution/InventoryWebApi/Controllers/InventoryStoreController.cs
@@ -1,37 +1,40 @@
-﻿using EntitiesBL.ModelEntities.CommonBL;
+﻿using CommonBL.Models;
 using EntitiesBL.ModelEntities.GeneratedEnitities;
 using InventoryWebAPI.Controllers;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace InventoryWebApi.Controllers
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class InventoryStoreController : BaseController<InventoryStoreCu>
-    {
-        public InventoryStoreController(IWebHostEnvironment env, InventoryDbContext context) : base(env, context)
-        {
-        }
+	[Route("[controller]")]
+	[ApiController]
+	public class InventoryStoreController : BaseController<InventoryStoreCu>
+	{
+		public InventoryStoreController(IWebHostEnvironment env, InventoryDbContext context) : base(env, context)
+		{
+		}
 
-        [HttpGet("GetInventoryStores")]
-        public virtual async Task<ActionResult<List<GetInventoryStores>>> GetInventoryStores(long inventoryStoreID)
-        {
-            List<GetInventoryStores> result = await _context.ExecuteStoredProcedureAsync<GetInventoryStores>("GetInventoryStores", new() {
-                { "inventoryStoreID", inventoryStoreID }
-            });
+		//[HttpGet("GetInventoryStores")]
+		//public virtual async Task<ActionResult<List<GetInventoryStores>>> GetInventoryStores(long inventoryStoreID)
+		//{
+		//    List<GetInventoryStores> result = await _context.ExecuteStoredProcedureAsync<GetInventoryStores>("GetInventoryStores", new() {
+		//        { "inventoryStoreID", inventoryStoreID }
+		//    });
 
-            if(result == null || result.Count == 0)
-            {
-                return NotFound("No inventory stores found for the given ID.");
-            }
 
-            foreach (GetInventoryStores inventoryStore in result)
-            {
-                
-            }
 
-            return Ok(result);
-        }
-    }
+		//    return Ok(result);
+		//}
+
+		[HttpGet("GetAllIsOnDuty")]
+		public override async Task<ActionResult<List<InventoryStoreCu>>> GetAllIsOnDuty()
+		{
+			ActionResult<List<InventoryStoreCu>> entitiesList = await base.GetAllIsOnDuty();
+
+			List<InventoryStoreCu>? stores = (entitiesList.Result as OkObjectResult)?.Value as List<InventoryStoreCu>;
+
+			List<InventoryStoreModel> inventoryStoresList = CommonBL.Libs.CommonBL.GetInventoryStores(_context, stores);
+
+			return Ok(inventoryStoresList);
+		}
+	}
 }
