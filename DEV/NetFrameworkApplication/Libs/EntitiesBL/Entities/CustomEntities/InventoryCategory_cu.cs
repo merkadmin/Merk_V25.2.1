9b44e497fb6cc579bf1dccf5480b4715bd731ae9
rayadramez82 <148684166+rayadramez82@users.noreply.github.com>
2025-09-19 00:48:30 +0300

    gh

---
+++ DEV/NetFrameworkApplication/Libs/EntitiesBL/Entities/CustomEntities/InventoryCategory_cu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EntitiesBL.EntitiesCommonBL;
+
+namespace EntitiesBL
+{
+	public partial class InventoryCategory_cu : IDBCommon
+	{
+		public long EntityID => ID;
+		public DB_CommonTransactionType DBCommonTransactionType { get; set; }
+		long IDBCommon.InsertedBy { get; set; }
+		DateTime IDBCommon.InsertedDate { get; set; }
+	}
+}
